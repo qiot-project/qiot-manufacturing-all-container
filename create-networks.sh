6818a2f82b8305b3cc5e10ee900f30929e9f1b61
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-07-08 11:50:38 +0200

    Base commit Added basic architectural components

---
+++ create-networks.sh
@@ -0,0 +1,5 @@
+docker network create manufacturing-datacenter;
+docker network create manufacturing-datacenterconnect;
+docker network create manufacturing-factory;
+docker network create manufacturing-factoryconnect;
+docker network create manufacturing-edge;
