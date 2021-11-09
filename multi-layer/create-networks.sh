4bb5b065d19207c8fb4b9479aa2e4eeeb04e6275
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-09 09:31:43 +0100

    Organize files into sub-folders

---
+++ multi-layer/create-networks.sh
@@ -0,0 +1,5 @@
+docker network create demo-manufacturing-datacenter;
+docker network create demo-manufacturing-datacenterconnect;
+docker network create demo-manufacturing-factory;
+docker network create demo-manufacturing-factoryconnect;
+docker network create demo-manufacturing-edge;
